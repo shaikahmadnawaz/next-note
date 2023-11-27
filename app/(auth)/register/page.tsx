3b278bf25869b311799665c63486865394a4cc69
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-11-28 01:18:20 +0530

    still working on authentication

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -2,13 +2,21 @@ import { UserAuthForm } from "@/components/form/user-auth-form";
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+// import { getServerSession } from "next-auth/next";
+// import { authOptions } from "@/lib/auth";
+// import { redirect } from "next/navigation";
 
 export const metadata = {
   title: "Register",
   description: "Register for an account to get started.",
 };
 
-export default function RegisterPage() {
+export default async function RegisterPage() {
+  // const session = await getServerSession(authOptions);
+  // console.log(session);
+  // if (session) {
+  //   return redirect("/dashboard");
+  // }
   return (
     <div className="container h-screen w-screen flex flex-col items-center justify-center">
       <Link href="/" className="absolute left-4 top-4 md:left-8 md:top-8">
