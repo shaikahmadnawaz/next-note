02c21d2cd5942c15eb9e4ea18995e6d3d611a65a
Shaik Ahmad Nawaz <nawazcode@gmail.com>
2023-11-29 00:27:41 +0530

    feat: authentication completed using github and email

--- components/form/user-auth-form.tsx
+++ components/form/user-auth-form.tsx
@@ -58,7 +58,7 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form method="post" action="/api/auth/sigin" onSubmit={handleSubmit(onSubmit)}>
         <div className="grid gap-2">
           <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
